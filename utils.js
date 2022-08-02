3763b7a6f85ea1baadd4fae3d10e6b350d12c97d
Partha <parthibanshiva@gmail.com>
2022-08-02 11:13:41 +0530

    *.json

--- utils.js
+++ utils.js
@@ -1,21 +1,21 @@
 const milliSecond = require("./globals");
 
-function time(ms){
-    return new Promise((resolve,reject)=>{
-        setTimeout(resolve,ms);
+function time(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
     });
 }
-async function print(){
+async function print() {
     await time(1000);
     console.log("HELLO WORLD!!!");
     // setInterval(()=>{
     //     print();
     // },2000);
 }
-async function call_print(){
-    setInterval(()=>{
+async function call_print() {
+    setInterval(() => {
         print();
-    },milliSecond.ms1);
+    }, milliSecond.ms1);
 }
 
-module.exports = {call_print,print,time};
+module.exports = { call_print, print, time };
